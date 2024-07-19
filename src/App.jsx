1702332900c5f81ad06ee928f926c353355fad6a
Alka Sah <alkasah485@gmail.com>
2024-07-18 22:48:46 -0700

    modification in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './App.css'
 //----------------------------------------import dependency
 import RecipeCard from './components/RecipeCard'
 import SearchBar from './components/SearchBar'
 //------------------------------------------import components
 const apiUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+
 function App() {
+  const[isLoading, setisLoading] = useState(false);
+  const [query, setQuery] = useState('');
+  const[recipe, setRecipe] = useState([]);
+
+  //search Recipe function
+
+  const searchRecipe = async () => {
+    setisLoading(true);
+    
+
+  }
  
 
   return (
