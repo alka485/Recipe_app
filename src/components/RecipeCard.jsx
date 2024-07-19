72353b137023a4189ef2a650c8937853dcb729f5
Alka Sah <alkasah485@gmail.com>
2024-07-18 22:34:12 -0700

    added components

---
+++ src/components/RecipeCard.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const RecipeCard = () => {
+  return (
+    <div>RecipeCard</div>
+  )
+}
+
+export default RecipeCard
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const SearchBar = () => {
+  return (
+    <div>SearchBar</div>
+  )
+}
+
+export default SearchBar
